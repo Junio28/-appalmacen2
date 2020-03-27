92a17000d9ee896ead47bd4bc285c1a163896ceb
Junior28 <ejuniormartinez@uniguajira.edu.co>
2020-03-27 06:01:28 -0500

    Commit Inicial2

---
+++ appalmacen_personalizado/appalmacen_mysql_pesonalizado/apps/clients/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class ClientsConfig(AppConfig):
+    name = 'clients'
+    name = 'apps.clients'
+    verbose_name = 'Gestor de Clientes'
---
+++ appalmacen_personalizado/appalmacen_postgres_personalizado/apps/products/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class ProductsConfig(AppConfig):
+    name = 'products'
+    name = 'apps.products'
+    verbose_name = 'Gestor de Productos'
